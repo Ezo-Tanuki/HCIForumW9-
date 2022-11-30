afeb46c396c808bb94ef800be691c9be5663f81e
Ëzo-Tanuki <michailbryanlie@gmail.com>
2022-11-30 13:34:24 +0700

    update

--- solution.js
+++ solution.js
@@ -1,17 +1,17 @@
 const digitalDecipher = (eMessage, key) => {
-    let inputArr = Array.from(eMessage);
+    let input = eMessage;
 
-    let numberFormat = inputArr.every(element => {
+    let numberFormat = input.every(element => {
         return typeof element === 'number'
     })
 
     key = Array.from(String(key));
     key.forEach((element, index, key) => key[index] = parseInt(element));
 
-    if (!numberFormat) inputArr.forEach((element, index) => inputArr[index] = element.charCodeAt());
-    else inputArr.forEach((element, index, inputArr) => inputArr[index] = element+96);
+    if (!numberFormat) input.forEach((element, index, input) => input[index] = element.charCodeAt());
+    else input.forEach((element, index, input) => input[index] = element+96);
 
-    let output = Array.from(inputArr, (element, index) => {
+    let output = Array.from(input, (element, index) => {
         element = (element - 97 - key[index%key.length])%26;
         return String.fromCharCode(element + 97);
     })
