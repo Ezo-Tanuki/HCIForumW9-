8cd85e772cf76e8189200e4e91c8a2bedc054a0a
Ëzo-Tanuki <michailbryanlie@gmail.com>
2022-11-30 20:44:44 +0700

    Quick fix

--- solution.js
+++ solution.js
@@ -12,13 +12,13 @@ const digitalDecipher = (eMessage, key) => {
     else input.forEach((element, index, input) => input[index] = element+96);
 
     let output = Array.from(input, (element, index) => {
-        element = (element - 97 - key[index%key.length])%26;
+        element = (element - 97 - key[index%key.length] + 26)%26;
         return String.fromCharCode(element + 97);
     })
 
     return output.join('');
 }
 
-console.log(digitalDecipher([20, 12, 18, 30, 21], 1939));
+console.log(digitalDecipher([20, 12, 18, 30, 21] , 1939));
 console.log(digitalDecipher([14, 30, 11, 1, 20, 17, 18, 18], 1990));
 console.log(digitalDecipher([6, 4, 1, 3, 9, 20], 100));
